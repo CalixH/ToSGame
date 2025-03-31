8e322798c020b298f58e2146d5d25a4cc089c1bc
CalixH <c295huan@uwaterloo.ca>
2025-03-31 13:38:15 -0400

    gNWOU:ESRHG

---
+++ tosgame/src/NewScreen.jsx
@@ -0,0 +1,10 @@
+// NewScreen.tsx (Destination screen)
+function NewScreen() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl">New Screen</h1>
+    </div>
+  );
+}
+
+export default NewScreen;
