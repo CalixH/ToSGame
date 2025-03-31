49325187e497e0c8a019fb592f4fbec19a688564
Jimmy Yang <jimmyy1337@gmail.com>
2025-03-31 14:52:14 -0400

    add tailwind

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,6 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./Home.css";
 
 function Home() {
   const [count, setCount] = useState(0);
@@ -10,32 +9,12 @@ function Home() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={() => navigate("/nav-screen")}
-        >
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <h1 className="font-bold text-3xl">ToS Game</h1>
+        <button className="w-24" onClick={() => navigate("/nav-screen")}>
           Start
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
